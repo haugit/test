4ffc8f110350ae9174a6f0299581c9be10d5d4c9
Henrik <Henrik@192.168.1.41>
2014-03-30 10:34:42 +0200

    commit 1

---
+++ src/gittest/pkg2/separate/maskiner/GitTest2SeparateMaskinerApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gittest.pkg2.separate.maskiner;
+
+/**
+ *
+ * @author Henrik
+ */
+public class GitTest2SeparateMaskinerApp
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hej 1");
+    }
+    
+}
